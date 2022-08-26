4399637db9930562f76f50980fd2b9352b76ffe8
Ron <duntoct29@gmail.com>
2022-08-26 20:54:34 +0700

    change

--- script.js
+++ script.js
@@ -19,24 +19,28 @@ window.onload=function(){
             }
         let note=document.getElementById('note');
         let note_data=note.value;
-        alert('Đẩy dữ liệu lên sheet thành công. Reload trang để dữ liệu được cập nhật');
-        let data={
-            'entry.1489518241': name_data,
-            'entry.1960618046': money_data,
-            'entry.1125466130':per_oke_data,
-            'entry.1916739674':note_data
+        if(money_data.length>0&&name_data.length>0){
+            alert('Đẩy dữ liệu lên sheet thành công. Reload trang để dữ liệu được cập nhật');
+            let data={
+                'entry.1489518241': name_data,
+                'entry.1960618046': money_data,
+                'entry.1125466130':per_oke_data,
+                'entry.1916739674':note_data
+            }
+            let queryString= new URLSearchParams(data);
+                queryString=queryString.toString()
+            const xhr = new XMLHttpRequest();
+            xhr.open("POST", 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSeqc9xCWKeSiwSsuu4AShn5EFG5TeJtpvf2x9eppZb3CWghpQ/formResponse', true);
+            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            xhr.send(queryString);
+    
+            name.value="";
+            per_oke.value="";
+            money.value="";
+            note.value="";
+        }else{
+            alert('Vui lòng nhập đầy đủ mục (*) bắt buộc.')
         }
-        let queryString= new URLSearchParams(data);
-            queryString=queryString.toString()
-        const xhr = new XMLHttpRequest();
-        xhr.open("POST", 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSeqc9xCWKeSiwSsuu4AShn5EFG5TeJtpvf2x9eppZb3CWghpQ/formResponse', true);
-        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        xhr.send(queryString);
-
-        name.value="";
-        per_oke.value="";
-        money.value="";
-        note.value="";
     }
 }
 $(document).ready(function(){
